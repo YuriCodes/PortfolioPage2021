e7ef526f395f740d595491c910d6953c534ea67f
yuri <yuricodesbot@gmail.com>
2022-01-06 01:12:32 -0400

    changed styles

--- script.js
+++ script.js
@@ -2,23 +2,24 @@
 consoleText(["Hello There.", "I'm Yuri.", "Front-End Developer."], 'text', ['#004d4d']);
 
 function consoleText(words, id, colors){
-    if(colors === undefined) colors = ['#fff'];
-    var visible = true;
+    if(colors === undefined) 
+    colors = ['#fff'];
+    
+    let letterCount = 1;
+    let x = 1;
+    let waiting = false;
+    let  target = document.getElementById(id)
 
-    var con = document.getElementById('console');
-    var letterCount = 1;
-    var x = 1;
-    var waiting = false;
-    var  target = document.getElementById(id)
     target.setAttribute('style', 'color:' + colors[0])
-    window.setInterval(function(){
-        if (letterCount === 0 && waiting === false) {
+    
+    window.setInterval(() => {
+        if (letterCount === 0 && waiting === false){
             waiting = true;
             target.innerHTML = words[0].substring(0, letterCount)
-            window.setTimeout(function(){
-                var usedColor = colors.shift();
+            window.setTimeout(() => {
+                let usedColor = colors.shift();
                 colors.push(usedColor)
-                var usedWord = words.shift();
+                let usedWord = words.shift();
                 words.push(usedWord)
                 x = 1;
                 target.setAttribute('style', 'color:' + colors[0])
@@ -27,7 +28,7 @@ function consoleText(words, id, colors){
             }, 120)
         } else if (letterCount === words[0].length + 1 && waiting === false){
             waiting = true;
-            window.setTimeout(function(){
+            window.setTimeout(() =>{
                 x = -1;
                 letterCount += x;
                 waiting = false;
@@ -37,15 +38,6 @@ function consoleText(words, id, colors){
             letterCount += x;
         }
     }, 120)
-    window.setInterval(function(){
-        if(visible === true){
-            con.className = `console-underscore-hidden`
-            visible = false;
-        } else {
-            con.className = `console-underscore`
-            visible = false;
-        }
-    }, 400)
 
 }
 
@@ -53,7 +45,9 @@ function consoleText(words, id, colors){
 mybutton = document.getElementById("buttonToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+    scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
